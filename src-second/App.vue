c3d6422b7f7a248872900438ee1a9d7f9890d2fa
zhangjiewei <1525456439@qq.com>
2020-03-13 17:12:46 +0800

    多页面和适配

---
+++ src-second/App.vue
@@ -0,0 +1,106 @@
+<template>
+    <div id="outer">
+        <table class="table2" border="1" cellspacing="0">
+            <thead>
+                <tr>
+                    <td colspan="6">xxx片组履行监督责任情况</td>
+                </tr>
+            </thead>
+            <tbody ref="tbo">
+                <tr>
+                    <td class="col_1">xx名称</td>
+                    <td colspan="5"></td>
+                </tr>
+                <tr>
+                    <td class="col_1">履责类型</td>
+                    <td colspan="5" class="fontLeft">①周两入企②月度履责③随手简督</td>
+                </tr>
+                <tr>
+                    <td class="col_1">履责情况</td>
+                    <td colspan="5"></td>
+                </tr>
+                <tr>
+                   <td rowspan="100">
+                       <p>随手简督</p>
+                       <p>发现问题</p>
+                   </td>
+                   <td>序号</td> 
+                   <td>问题对象（单位/个人）</td>
+                   <td>问题表现</td>
+                   <td>整改落实</td>
+                   <td>关联指标</td>
+                </tr>
+                <tr v-for="(item,index) in list" :key="index">
+                    <td>{{item.sequenceNumber}}</td>
+                    <td>{{item.questionObject}}</td>
+                    <td>{{item.quesetionManifestation}}</td>
+                    <td>{{item.zhenggailuoshi}}</td>
+                    <td>{{item.guanlianzhibiao}}</td>
+                </tr>
+            </tbody>
+        </table>
+        <button @click="addRow">添加一行</button>
+        <a href="./index.html">go index page</a>
+    </div>
+</template>
+
+<script>
+// const row_str11 = '<tr><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td></tr>';
+
+export default {
+    data(){
+        return {
+            list:[{sequenceNumber:'',questionObject:'',quesetionManifestation:'',zhenggailuoshi:'',guanlianzhibiao:''}]
+        }
+    },
+    methods:{
+        addRow:function(){
+            console.log(this.$refs.tbo.rows[4]);
+            console.log(this.$refs.tbo.rows)
+            this.list.push(this.list[0]);            
+        }
+    }
+
+}
+</script>
+
+<style lang='scss' scoped>
+    *{
+        margin: 0;
+        padding: 0;
+    }
+    @function px2rem($px){
+        @return $px/75 + rem;
+    }
+    #outer{
+        .table2{
+            margin: 0 auto;
+            width: px2rem(626);
+            text-align: center;
+            thead{
+                tr{
+                    td{
+                        font-size: 17px;
+                        font-weight: 700;
+                        height: px2rem(26);
+                    }
+                }
+            }
+            tbody{
+                font-size: 16px;
+                tr{
+                    .col_1{
+                        width: px2rem(117);
+                    }
+                    td{
+                        height: px2rem(30);
+                        width: px2rem(103);
+                    }
+                    .fontLeft{
+                        text-align: left;
+                    }
+                }
+            }
+        }
+    }
+</style>
---
+++ src/App.vue
@@ -0,0 +1,107 @@
+<template>
+  <div>
+    <div class="outer">
+      <table class="indexTable" border="1" cellspacing="0">
+        <thead>
+          <tr>
+            <td colspan="13">xx履行监督责任情况</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="colspan-1">xx名称</td>
+            <td colspan="12"></td>
+          </tr>
+          <tr>
+            <td>履责类型</td>
+            <td class="colspan-2" colspan="12">①月度履责②意见建议③讲党课④随手监督</td>
+          </tr>
+          <tr>
+            <td>履责情况</td>
+            <td colspan="12"></td>
+          </tr>
+          <tr rowspan="3">
+            <td>监督执纪情况（按月）</td>
+            <td colspan="2">廉政谈话数</td>
+            <td colspan="2">支部监督信息员报送数</td>
+            <td colspan="2">信访处理数</td>
+            <td colspan="2">问题线索处置数</td>
+            <td colspan="2">立案审查数</td>
+            <td colspan="2">运用四种形态数</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>新增</td>
+            <td>累计</td>
+            <td>新增</td>
+            <td>累计</td>
+            <td>新增</td>
+            <td>累计</td>
+            <td>新增</td>
+            <td>累计</td>
+            <td>新增</td>
+            <td>累计</td>
+            <td>新增</td>
+            <td>累计</td>
+          </tr>
+          <tr class="row-6">
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <a href="./second.html">go second tabble</a>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+  @import './assets/css/reset.scss';
+  .outer{   
+    margin: 50px auto;
+    .indexTable{
+      border: 1px solid black;
+      width: px2rem(630);
+      text-align: center;
+      tr{
+        td{
+          border: 1px solid black;
+          font-size: 16px;
+        }
+        .colspan-1{
+          width: px2rem(116);
+        }
+        .colspan-2{
+          text-align: left;
+        }
+      }
+      thead{
+        font-size: 18px;
+        font-weight: 700;
+        height: px2rem(30); 
+      }
+      tbody{
+        .row-6{
+          height: px2rem(52);
+        }
+      }
+    }
+  }
+</style>
